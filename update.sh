a88e92b64130aaac4ad6d806359f12e4b7dc6995
Rafael de F. Ferreira <rafael@rafaelferreira.net>
2015-11-23 17:05:42 -0200

    Update update

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 cp $HOME/scripts/setuphome.sh ./
-cp $HOME/scripts/setupnu.sh ./
+cp /home/rafael/dev/nu/nudev/setupnu.sh ./
 cp /etc/nixos/configuration.nix ./
 
 git commit -m "Updated nixos setup" -- setuphome.sh configuration.nix setupnu.sh
