b8efca94c5e33484adcb15f5801ebae3408dac4c
Rafael de F. Ferreira <rafael@rafaelferreira.net>
2017-01-25 17:30:49 -0200

    Copy patch files to /etc

--- install.sh
+++ install.sh
@@ -3,7 +3,9 @@ set -e
 
 HOST="$1"
 NIX_FILES=$(ls ./*.nix | grep -v configuration)
+PATCH_FILES=$(ls ./*.patch | grep -v configuration)
 CONFIG_FILE="./configuration-${HOST}.nix"
 
 cp $NIX_FILES /etc/nixos/
+cp $PATCH_FILES /etc/nixos/
 cp $CONFIG_FILE /etc/nixos/configuration.nix
