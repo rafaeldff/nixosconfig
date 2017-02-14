db20920a80642a18667034bad3478587cb288293
Rafael de F. Ferreira <rafael@rafaelferreira.net>
2017-02-14 14:47:20 -0200

    Update spotify

--- applications-packages.nix
+++ applications-packages.nix
@@ -9,14 +9,7 @@ in
     vimHugeX
     chromium
     firefox
-    (lib.overrideDerivation spotify (attrs: {
-       name = "spotify-${spotifyVersion}";
-      src =
-        fetchurl {
-          url = "http://repository-origin.spotify.com/pool/non-free/s/spotify-client/spotify-client_${spotifyVersion}_amd64.deb";
-          sha256 = "03r4hz4x4f3zmp6dsv1n72y5q01d7mfqvaaxqvd587a5561gahf0";
-        };
-    }))
+    spotify
     evince
     vlc
     libreoffice
