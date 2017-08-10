e44471f31318399816074e4ec854b149ed7d46f8
Rafael de F. Ferreira <rafael@rafaelferreira.net>
2017-08-10 12:58:53 -0300

    Update spotify, remove flash from chromium

--- applications-packages.nix
+++ applications-packages.nix
@@ -1,8 +1,8 @@
 { config, pkgs, ... }:
 
 let
-  #spotifyVersion = "1.0.36.120.g536a862f-20";
-  spotifyVersion =  "1.0.57.474.gca9c9538-30";
+  #spotifyVersion =  "1.0.57.474.gca9c9538-30";
+  spotifyVersion =  "1.0.59.395.ge6ca9946-18";
 in
 {
   environment.systemPackages =
@@ -15,7 +15,7 @@ in
       src =
         fetchurl {
           url = "http://repository-origin.spotify.com/pool/non-free/s/spotify-client/spotify-client_${spotifyVersion}_amd64.deb";
-          sha256 = "fe46f2084c45c756bee366f744d2821d79e82866b19942e30bb2a20c1e597437";
+          sha256 = "8fbe821fd516f5cd77eb2d5085e5acd8f1852b97f7b7294846fcc57b810ef515";
         };
     }))
     evince
@@ -26,7 +26,6 @@ in
 
   nixpkgs.config = {
     chromium = {
-      enablePepperFlash = true;
       enablePepperPDF = true;
     };
   };
