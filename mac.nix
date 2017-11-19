372a3c4d67d7324e69eb214c2686922661626065
Rafael de F. Ferreira <rafael@rafaelferreira.net>
2017-11-19 16:03:29 -0200

    Enable bluetooth headset

--- mac.nix
+++ mac.nix
@@ -11,6 +11,11 @@
   '';
 
   hardware.bluetooth.enable = true;
+  hardware.pulseaudio = {
+    enable = true;
+    package = pkgs.pulseaudioFull;
+  };
+  environment.systemPackages = [ pkgs.pavucontrol ];
 
   # Use the gummiboot efi boot loader.
   boot.loader.systemd-boot.enable = true;
