28b5dd6bf82c17f076142c10f0956c647ed1fcc6
Rafael de F. Ferreira <rafael@rafaelferreira.net>
2017-12-22 14:32:38 -0200

    Bump spotify again

--- applications-packages.nix
+++ applications-packages.nix
@@ -1,7 +1,8 @@
 { config, pkgs, ... }:
 
 let
-  #spotifyVersion =  "1.0.59.395.ge6ca9946-18";
+  #spotifyVersion = "1.0.59.395.ge6ca9946-18";
+  spotifyVersion  = "1.0.69.336.g7edcc575-39";
 in
 {
   environment.systemPackages =
@@ -9,15 +10,15 @@ in
     vimHugeX
     chromium
     firefox
-    #(lib.overrideDerivation spotify (attrs: {
-       #name = "spotify-${spotifyVersion}";
-      #src =
-        #fetchurl {
-          #url = "http://repository-origin.spotify.com/pool/non-free/s/spotify-client/spotify-client_${spotifyVersion}_amd64.deb";
-          #sha256 = "8fbe821fd516f5cd77eb2d5085e5acd8f1852b97f7b7294846fcc57b810ef515";
-        #};
-    #}))
-    spotify
+    (lib.overrideDerivation spotify (attrs: {
+       name = "spotify-${spotifyVersion}";
+      src =
+        fetchurl {
+          url = "http://repository-origin.spotify.com/pool/non-free/s/spotify-client/spotify-client_${spotifyVersion}_amd64.deb";
+          sha256 = "0bh2q7g478g7wj661fypxcbhrbq87zingfyigg7rz1shgsgwc3gd";
+        };
+    }))
+    #spotify
     evince
     vlc
     libreoffice
