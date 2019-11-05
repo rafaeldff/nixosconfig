16d259778680971e0ca30586c6d2defb738e3096
Rafael de F. Ferreira <rafael@rafaelferreira.net>
2019-11-05 12:03:25 -0200

    Revert "Try to configure mac facetime camera"
    This reverts commit e3ce51d3c06270381e5ac4e89443b31456035f1a.

--- mac.nix
+++ mac.nix
@@ -33,29 +33,6 @@
   };
 
   services.xserver.config = " # adding another comment";
-  hardware.facetimehd.enable = true;
-
-  nixpkgs.config.packageOverrides = super:
-  let
-    osxBlob = pkgs.requireFile {
-      message = ''
-      nix-prefetch-url file:///home/rafael/nixosconfig/OSXUpd10.11.5.dmg 
-      '';
-      #sha256 = "46cd31ee35b084f59dc8b8f632e6bebdb4badeafbab064eea32ae66cc3743301";
-      sha256 = "009kfk1nrrialgp69c5smzgbmd5xpvk35xmqr2fzb15h6pp33ka6";
-      #sha256 = "0zb52vsv04if2kla5k2azwfi04mn8mmpl5ahkgpch0157byygb4x";
-      name = "AppleCameraInterface";
-      };
-      in {
-      facetimehd-firmware = super.facetimehd-firmware.overrideDerivation (old: {
-      name = "facetimehd-firmware";
-      src = null; # disables download from apple
-      buildPhase = ''
-      mkdir -p $out/lib/firmware/facetimehd
-      dd bs=1 skip=81920 if=${osxBlob} | gunzip -c > $out/lib/firmware/facetimehd/firmware.bin || true
-      '';
-    });
-  };
 
 }
 
