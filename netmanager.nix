b71237c33f4d889f1cd55ff36d59b8b60c6446a9
Rafael de F. Ferreira <rafael@rafaelferreira.net>
2020-09-04 19:07:27 +0200

    Update networking config (per interface dhcp)

--- netmanager.nix
+++ netmanager.nix
@@ -2,7 +2,13 @@
 
 {
   networking.networkmanager.enable = true;
-  networking.wireless.enable = false;
-  networking.firewall.enable = false;
-  #networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
+  # networking.wireless.enable = false;
+  # networking.firewall.enable = false;
+  # networking.nameservers = [ "8.8.8.8" "8.8.4.4" ];
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so look for interface config on
+  # hardware-specific config file (e.g. nu-dell.nix)
+  networking.useDHCP = false;
+
 }
