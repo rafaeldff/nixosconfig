c818dda6406e8e65ab772894b9653e4e6fe1cc91
Rafael de F. Ferreira <rafael.ferreira@nubank.com.br>
2022-05-16 11:31:22 +0200

    Set up cargo local bin path

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -6,11 +6,17 @@ sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfi
 cargo install alacritty 
 
 cat << 'EOF' >> $HOME/.profile
-#
+
 # Source home-manager init script
 if [ -f $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh ]; then
   . $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
 fi
-#
+
+# Add cargo local bin to PATH (for, e.g., alacritty)
+if [ -d "$HOME/.cargo/bin" ] ; then
+    PATH="$HOME/.cargo/bin:$PATH"
+fi
+
+
 EOF
 
