39924f3f05ce59743fefae3315089ceb233a3fec
Rafael de F. Ferreira <rafael.ferreira@nubank.com.br>
2022-05-16 11:04:23 +0200

    Append hm init to .profile

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -5,7 +5,7 @@ sudo cp default.desktop $GDM_XSESSIONS_DIR
 sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 cargo
 cargo install alacritty 
 
-cat << 'EOF' >> /tmp/testing
+cat << 'EOF' >> $HOME/.profile
 #
 # Source home-manager init script
 if [ -f $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh ]; then
