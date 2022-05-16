d96af93eb5dd1e49a7d64dd622e47e38e0037960
Rafael de F. Ferreira <rafael.ferreira@nubank.com.br>
2022-05-16 10:58:35 +0200

    Append home-manager init to .bashrc

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -5,3 +5,12 @@ sudo cp default.desktop $GDM_XSESSIONS_DIR
 sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 cargo
 cargo install alacritty 
 
+cat << 'EOF' >> /tmp/testing
+#
+# Source home-manager init script
+if [ -f $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh ]; then
+  . $HOME/.nix-profile/etc/profile.d/hm-session-vars.sh
+fi
+#
+EOF
+
