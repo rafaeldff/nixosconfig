fbd103cae8dcdfb4415c91956d0fee8019191480
Rafael de F. Ferreira <rafael.ferreira@nubank.com.br>
2022-06-16 20:52:30 +0200

    Ensure home-manager in ubuntu uses cargo's alacritty

--- home/xmonad-environment-packages.nix
+++ home/xmonad-environment-packages.nix
@@ -4,7 +4,7 @@
   home.packages =
     with pkgs; [
     termite
-    alacritty
+    #alacritty # commented out because it has to be installed from sources in ubuntu
     haskellPackages.xmobar
     dmenu
     rxvt_unicode
