593a1bd3b0dc7b57dcb450a2af06f673a252053e
Rafael de F. Ferreira <rafael@rafaelferreira.net>
2026-02-27 14:52:48 +0100

    Required changes for latest update

--- platform.nix
+++ platform.nix
@@ -1,11 +1,15 @@
 {config, pkgs, ...}:
 
 {
-  # kernel version (latest for net and audio drivers for fw laptop)
-  boot.kernelPackages =  pkgs.linuxPackages_latest;
+  # kernel version (used to be latest for net and audio drivers for fw laptop)
+  # set to 6_18 because 6_19 (latest as of last run) was incompatible with VirtualBox
+  boot.kernelPackages =  pkgs.linuxPackages_6_18;
 
   # The NixOS release to be compatible with for stateful data such as databases.  # system.stateVersion = "15.09";
   
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.oraclejdk.accept_license = true;
+
+  # We were running out of space on the /boot partition, so had to limit the number of linked configurations
+  boot.loader.systemd-boot.configurationLimit = 3;  # or 3 if it fits
 }
